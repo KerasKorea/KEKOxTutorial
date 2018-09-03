f5b7887625da7880b49595a889f11c8020370a27
mike2ox <firefinger07@gmail.com>
2018-09-03 11:47:58 +0900

    #25 : 기본 주석 한글화 완료, code line별 첨가 주석을 붙여줘야함

--- 25_Keras_examples_(3)_Generative_models_examples/neural_style_transfer.py
+++ 25_Keras_examples_(3)_Generative_models_examples/neural_style_transfer.py
@@ -1,54 +1,48 @@
-'''Neural style transfer with Keras.
+'''케라스로 신경 스타일로 바꾸기
 
-Run the script with:
+현 스크립트를 실행하기 위해선:
 ```
 python neural_style_transfer.py path_to_your_base_image.jpg \
     path_to_your_reference.jpg prefix_for_results
 ```
-e.g.:
+예 :
 ```
 python neural_style_transfer.py img/tuebingen.jpg \
     img/starry_night.jpg results/my_result
 ```
-Optional parameters:
+추가 파라미터들은 :
 ```
---iter, To specify the number of iterations \
-    the style transfer takes place (Default is 10)
---content_weight, The weight given to the content loss (Default is 0.025)
---style_weight, The weight given to the style loss (Default is 1.0)
---tv_weight, The weight given to the total variation loss (Default is 1.0)
+--iter, 스타일 변화를 수행하는 반복횟수를 지정함.(고정값은 10)
+--content_weight, 컨텐츠 손실에 대한 가중치 (고정값은 0.025)
+--style_weight, 스타일 손실에 대한 가중치 (고정값은 1.0)
+--tv_weight, 전체 변화 손실에 대한 가중치 (고정값은 1.0)
 ```
 
-It is preferable to run this script on GPU, for speed.
+속도를 위해, GPU에서 현 스크립트를 실행하길 권합니다.
 
-Example result: https://twitter.com/fchollet/status/686631033085677568
+예시에 대한 결과 : https://twitter.com/fchollet/status/686631033085677568
 
-# Details
+# 상세설명
 
-Style transfer consists in generating an image
-with the same "content" as a base image, but with the
-"style" of a different picture (typically artistic).
+스타일 변형은 기존 이미지와 동일한 '콘텐츠'를 사용하여
+이미지를 생성하지만 다른 이미지의 '스타일'을 갖도록 생성합니다.
 
-This is achieved through the optimization of a loss function
-that has 3 components: "style loss", "content loss",
-and "total variation loss":
+손실 함수의 최적화를 통해 얻을 수 있으며, 
+이는 3가지 요소를 가집니다.: "style loss", "content loss", "total variation loss"
 
-- The total variation loss imposes local spatial continuity between
-the pixels of the combination image, giving it visual coherence.
+ - 전체 변화 손실(total variation loss)은 조합 이미지의 픽셀들 사이에
+local 공간 연속성을 부과하여 시각적 일관화를 제공합니다. 
 
-- The style loss is where the deep learning keeps in --that one is defined
-using a deep convolutional neural network. Precisely, it consists in a sum of
-L2 distances between the Gram matrices of the representations of
-the base image and the style reference image, extracted from
-different layers of a convnet (trained on ImageNet). The general idea
-is to capture color/texture information at different spatial
-scales (fairly large scales --defined by the depth of the layer considered).
+ - 스타일 손실(The style loss)은 딥러닝이 시행되는 구간으로, 이는 deep CNN을 사용하여
+  정의합니다. 정확히는, (ImageNet으로 훈련된) 다른 계층들에서 추출된, 
+  기본 이미지의 표현값들의 Gram matrix와 스타일 기준 이미지간의 L2 거리의 합으로 구성됩니다. 
+   일반적인 아이디어는 색상/질감 정보를 다양한 공간적 척도(scale)
+   (꽤나 큰 구조 -- 언급된 계층의 깊이로 정의)로 수집하는 겁니다.
 
- - The content loss is a L2 distance between the features of the base
-image (extracted from a deep layer) and the features of the combination image,
-keeping the generated image close enough to the original one.
+ - 콘텐츠 손실(The content loss)은 기본 이미지 특징과 결합 이미지 간의 L2 거리로,
+ 생성된 이미지가 원본에 가까워 지도록 해줍니다.
 
-# References
+# 참고 자료
     - [A Neural Algorithm of Artistic Style](http://arxiv.org/abs/1508.06576)
 '''
 
@@ -84,18 +78,17 @@ style_reference_image_path = args.style_reference_image_path
 result_prefix = args.result_prefix
 iterations = args.iter
 
-# these are the weights of the different loss components
+# 서로 다른 손실 요소들의 가중치들을 가져옵니다.
 total_variation_weight = args.tv_weight
 style_weight = args.style_weight
 content_weight = args.content_weight
 
-# dimensions of the generated picture.
+# 생성할 이미지의 차원을 설정합니다.
 width, height = load_img(base_image_path).size
 img_nrows = 400
 img_ncols = int(width * img_nrows / height)
 
-# util function to open, resize and format pictures into appropriate tensors
-
+# 아래 함수는 이미지를 열어서 크기를 조정하고 적절한 tensor로 format시킵니다.
 
 def preprocess_image(image_path):
     img = load_img(image_path, target_size=(img_nrows, img_ncols))
@@ -104,8 +97,7 @@ def preprocess_image(image_path):
     img = vgg19.preprocess_input(img)
     return img
 
-# util function to convert a tensor into a valid image
-
+# 아래 함수는 tensor를 검증 이미지로 전환시킵니다.
 
 def deprocess_image(x):
     if K.image_data_format() == 'channels_first':
@@ -113,7 +105,7 @@ def deprocess_image(x):
         x = x.transpose((1, 2, 0))
     else:
         x = x.reshape((img_nrows, img_ncols, 3))
-    # Remove zero-center by mean pixel
+    # 평균 픽셀을 기준으로 zero중심 제거
     x[:, :, 0] += 103.939
     x[:, :, 1] += 116.779
     x[:, :, 2] += 123.68
@@ -122,35 +114,35 @@ def deprocess_image(x):
     x = np.clip(x, 0, 255).astype('uint8')
     return x
 
-# get tensor representations of our images
+
+# 주어진 이미지를 tensor형식으로 갖는다.
 base_image = K.variable(preprocess_image(base_image_path))
 style_reference_image = K.variable(preprocess_image(style_reference_image_path))
 
-# this will contain our generated image
+# 생성될 이미지를 갖도록 설정합니다.
 if K.image_data_format() == 'channels_first':
     combination_image = K.placeholder((1, 3, img_nrows, img_ncols))
 else:
     combination_image = K.placeholder((1, img_nrows, img_ncols, 3))
 
-# combine the 3 images into a single Keras tensor
+# 3가지 이미지를 1개의 keras tensor로 결합시킵니다.
 input_tensor = K.concatenate([base_image,
                               style_reference_image,
                               combination_image], axis=0)
 
-# build the VGG16 network with our 3 images as input
-# the model will be loaded with pre-trained ImageNet weights
+# (갖고있는)3가지 이미지를 입력으로하는 VGG16 네트워크를 구성합니다.
+# 해당 model은 미리 ImageNet으로 학습된 가중치를 갖습니다.
 model = vgg19.VGG19(input_tensor=input_tensor,
                     weights='imagenet', include_top=False)
 print('Model loaded.')
 
-# get the symbolic outputs of each "key" layer (we gave them unique names).
+# 각 핵십 계층의 상징적 결과들을 갖습니다(그 결과들에 이름을 지정합니다.)
 outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
 
-# compute the neural style loss
-# first we need to define 4 util functions
+# 신경 스타일 손실을 계산합니다.
+# 먼저 4가지 함수들을 정의할 필요가 있습니다.
 
-# the gram matrix of an image tensor (feature-wise outer product)
-
+# 이미지 tensor의 gram matrix(feature-wise 외적연산)
 
 def gram_matrix(x):
     assert K.ndim(x) == 3
@@ -161,11 +153,8 @@ def gram_matrix(x):
     gram = K.dot(features, K.transpose(features))
     return gram
 
-# the "style loss" is designed to maintain
-# the style of the reference image in the generated image.
-# It is based on the gram matrices (which capture style) of
-# feature maps from the style reference image
-# and from the generated image
+# 스타일 손실은 생성된 이미지에서 기존 이미지의 스타일을 유지하도록 설계됩니다.
+# 스타일 기준 이미지와 생성된 이미지에서 특징에 대한 gram matrix(스타일 수집)를 기반으로 합니다.
 
 
 def style_loss(style, combination):
@@ -177,17 +166,15 @@ def style_loss(style, combination):
     size = img_nrows * img_ncols
     return K.sum(K.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
 
-# an auxiliary loss function
-# designed to maintain the "content" of the
-# base image in the generated image
+# 보조 손실 함수는 생성된 이미지에서 
+# 기존 이미지의 '콘텐츠'를 유지하도록 설계되었습니다.
 
 
 def content_loss(base, combination):
     return K.sum(K.square(combination - base))
 
-# the 3rd loss function, total variation loss,
-# designed to keep the generated image locally coherent
-
+# 3번째 손실함수인 전체 변화 손실(total variation loss)은 
+# 생성된 이미지를 지역적으로 일관성있게 유지되도록 설계되었습니다.
 
 def total_variation_loss(x):
     assert K.ndim(x) == 4
@@ -203,7 +190,7 @@ def total_variation_loss(x):
             x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, :img_nrows - 1, 1:, :])
     return K.sum(K.pow(a + b, 1.25))
 
-# combine these loss functions into a single scalar
+# 이런 손실 함수들을 단일 숫자(스칼라)로 결합시킵니다.
 loss = K.variable(0.)
 layer_features = outputs_dict['block5_conv2']
 base_image_features = layer_features[0, :, :, :]
@@ -222,7 +209,7 @@ for layer_name in feature_layers:
     loss += (style_weight / len(feature_layers)) * sl
 loss += total_variation_weight * total_variation_loss(combination_image)
 
-# get the gradients of the generated image wrt the loss
+# 손실에 의해 생성된 이미지의 기울기를 갖습니다.
 grads = K.gradients(loss, combination_image)
 
 outputs = [loss]
@@ -247,13 +234,10 @@ def eval_loss_and_grads(x):
         grad_values = np.array(outs[1:]).flatten().astype('float64')
     return loss_value, grad_values
 
-# this Evaluator class makes it possible
-# to compute loss and gradients in one pass
-# while retrieving them via two separate functions,
-# "loss" and "grads". This is done because scipy.optimize
-# requires separate functions for loss and gradients,
-# but computing them separately would be inefficient.
-
+# Evaluator 클래스는 '손실'과 '기울기'라는 2가지 별도 함수들을 통해 
+# 한번으로 손실과 기울기를 검색하는 동시에 계산을 할 수 있도록 합니다.
+# 왜 그렇게 진행했냐면 scipy.optimize는 손실과 기울기에 대한 별도의 함수를
+# 요구하지만 따로 계산할 경우 비효율적일수 있기 때문입니다.
 
 class Evaluator(object):
 
@@ -277,8 +261,8 @@ class Evaluator(object):
 
 evaluator = Evaluator()
 
-# run scipy-based optimization (L-BFGS) over the pixels of the generated image
-# so as to minimize the neural style loss
+# 신결 스타일 손실을 최소화하기 위해
+# 생성된 이미지 픽셀들을 scipy기반으로 최적화(L-BFGS)합니다.
 x = preprocess_image(base_image_path)
 
 for i in range(iterations):
@@ -287,7 +271,7 @@ for i in range(iterations):
     x, min_val, info = fmin_l_bfgs_b(evaluator.loss, x.flatten(),
                                      fprime=evaluator.grads, maxfun=20)
     print('Current loss value:', min_val)
-    # save current generated image
+    # 현재 생성된 이미지를 저장합니다.
     img = deprocess_image(x.copy())
     fname = result_prefix + '_at_iteration_%d.png' % i
     save_img(fname, img)
