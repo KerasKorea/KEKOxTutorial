7c2975b0681448f9705504373eb29e1832488b90
mike2ox <firefinger07@gmail.com>
2018-10-01 22:59:49 +0900

    #25 : code 실행을 위해 폴더 이름 변경경

--- 25_Keras_examples_3_Generative_models_examples/variational_autoencoder_deconv.py
+++ 25_Keras_examples_3_Generative_models_examples/variational_autoencoder_deconv.py
@@ -1,11 +1,4 @@
 '''Example of VAE on MNIST dataset using CNN
-CNN을 활용하여 MNIST 데이터 세트기반의 VAE 예제
-
-VAE는 모듈러(modular) 설계를 가집니다. 인코더(encoder), 디코더(decoder) 그리고 VAE(다양한 오토인코더(auto encoder))는
-가중치를 공유하는 3가지 모델입니다. VAE 모델을 학습시키고,
-인코더는 latent 벡터들을 생성하는데 사용됩니다.
-디코더는 평균이 0이고 편차가 1인 가우시안 분포로 부터 latent vector를 샘플링함으로서
-MNIST digits를 생성하는데 사용됩니다.
 
 The VAE has a modular design. The encoder, decoder and VAE
 are 3 models that share weights. After training the VAE model,
@@ -13,8 +6,7 @@ the encoder can be used to  generate latent vectors.
 The decoder can be used to generate MNIST digits by sampling the
 latent vector from a Gaussian distribution with mean=0 and std=1.
 
-
-# 참고 문헌
+# Reference
 
 [1] Kingma, Diederik P., and Max Welling.
 "Auto-encoding variational bayes."
@@ -41,22 +33,15 @@ import os
 
 
 # reparameterization trick
-# 재(re)-매개변수화 기법
-# Q(z|X)에서 샘플링하지 않고 N(0,I)에서 샘플링(eps)합니다.
-# 그때 Z는 z_mean + sqrt(var)*eps
 # instead of sampling from Q(z|X), sample eps = N(0,I)
 # then z = z_mean + sqrt(var)*eps
 def sampling(args):
     """Reparameterization trick by sampling fr an isotropic unit Gaussian.
-    # TODO : 재(re)-매개변수화 기법 : isotropic 단일 가우시안에서 샘플링합니다.
+
     # Arguments:
-    # 설명:
-        args (텐서) : Q(z|X)의 분산의 log취한 값과 평균
         args (tensor): mean and log of variance of Q(z|X)
 
     # Returns:
-    # 반환:
-        z (텐서) : 샘플링된 latent 벡터
         z (tensor): sampled latent vector
     """
 
@@ -64,7 +49,6 @@ def sampling(args):
     batch = K.shape(z_mean)[0]
     dim = K.int_shape(z_mean)[1]
     # by default, random_normal has mean=0 and std=1.0
-    # 고정적으로, random_normal은 평균 0, 표준편차 1.0을 가진다.
     epsilon = K.random_normal(shape=(batch, dim))
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
@@ -74,13 +58,12 @@ def plot_results(models,
                  batch_size=128,
                  model_name="vae_mnist"):
     """Plots labels and MNIST digits as function of 2-dim latent vector
-    2차원 축소 벡터의 함수로써 레이블과 MNIST 자릿수들을 표기한다.
 
-    # 입력값들:
-        models (튜플(tuple)): 인코더, 디코더 모델들
-        data (튜플): 테스트 데이터와 레이블
-        batch_size (정수): 예측 배치 사이즈
-        model_name (문자열): 이 함수에서 쓸 모델 이름
+    # Arguments:
+        models (tuple): encoder and decoder models
+        data (tuple): test data and label
+        batch_size (int): prediction batch size
+        model_name (string): which model is using this function
     """
 
     encoder, decoder = models
@@ -89,7 +72,6 @@ def plot_results(models,
 
     filename = os.path.join(model_name, "vae_mean.png")
     # display a 2D plot of the digit classes in the latent space
-    # 잠재 공간에 있는 자릿수 클래스들의 2차원 표시를 보여준다.
     z_mean, _, _ = encoder.predict(x_test,
                                    batch_size=batch_size)
     plt.figure(figsize=(12, 10))
@@ -102,13 +84,11 @@ def plot_results(models,
 
     filename = os.path.join(model_name, "digits_over_latent.png")
     # display a 30x30 2D manifold of digits
-    # 30x30 크기의 2차원 자릿수들을 보여준다.
     n = 30
     digit_size = 28
     figure = np.zeros((digit_size * n, digit_size * n))
     # linearly spaced coordinates corresponding to the 2D plot
     # of digit classes in the latent space
-    # TODO : 잠재 공간에 
     grid_x = np.linspace(-4, 4, n)
     grid_y = np.linspace(-4, 4, n)[::-1]
 
@@ -136,7 +116,6 @@ def plot_results(models,
 
 
 # MNIST dataset
-# MNIST 데이터세트를 불러옵니다.
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 image_size = x_train.shape[1]
@@ -146,7 +125,6 @@ x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
 # network parameters
-# 네트워크를 구성하는 매개변수들
 input_shape = (image_size, image_size, 1)
 batch_size = 128
 kernel_size = 3
@@ -154,8 +132,8 @@ filters = 16
 latent_dim = 2
 epochs = 30
 
-# VAE 모델은 인코더와 디코더를 결합한 구조입니다.
-# 아래는 인코더 모델을 구성하는 과정입니다.
+# VAE model = encoder + decoder
+# build encoder model
 inputs = Input(shape=input_shape, name='encoder_input')
 x = inputs
 for i in range(2):
@@ -167,11 +145,9 @@ for i in range(2):
                padding='same')(x)
 
 # shape info needed to build decoder model
-# 디코더 모델을 구성하는데 필요한 데이터(x) 형태를 추출합니다.
 shape = K.int_shape(x)
 
 # generate latent vector Q(z|X)
-# 잠재 벡터 Q(z|X)를 생성합니다.
 x = Flatten()(x)
 x = Dense(16, activation='relu')(x)
 z_mean = Dense(latent_dim, name='z_mean')(x)
@@ -182,12 +158,11 @@ z_log_var = Dense(latent_dim, name='z_log_var')(x)
 z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
 
 # instantiate encoder model
-# 인코더 모델을 인스턴스(instantiate)합니다.
 encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
 encoder.summary()
 plot_model(encoder, to_file='vae_cnn_encoder.png', show_shapes=True)
 
-# 디코더 모델을 생성합니다.
+# build decoder model
 latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
 x = Dense(shape[1] * shape[2] * shape[3], activation='relu')(latent_inputs)
 x = Reshape((shape[1], shape[2], shape[3]))(x)
@@ -207,13 +182,11 @@ outputs = Conv2DTranspose(filters=1,
                           name='decoder_output')(x)
 
 # instantiate decoder model
-# 디코더 모델을 인스턴스 합니다.
 decoder = Model(latent_inputs, outputs, name='decoder')
 decoder.summary()
 plot_model(decoder, to_file='vae_cnn_decoder.png', show_shapes=True)
 
 # instantiate VAE model
-# VAE 모델을 인스턴스 합니다.
 outputs = decoder(encoder(inputs)[2])
 vae = Model(inputs, outputs, name='vae')
 
@@ -228,7 +201,6 @@ if __name__ == '__main__':
     data = (x_test, y_test)
 
     # VAE loss = mse_loss or xent_loss + kl_loss
-    # VAE 손실은 mes 손실 혹은 xent 손실과 kl 손실 간의 합과 같습니다.
     if args.mse:
         reconstruction_loss = mse(K.flatten(inputs), K.flatten(outputs))
     else:
@@ -248,7 +220,7 @@ if __name__ == '__main__':
     if args.weights:
         vae.load_weights(args.weights)
     else:
-        # 오토인코더를 학습시킵니다.
+        # train the autoencoder
         vae.fit(x_train,
                 epochs=epochs,
                 batch_size=batch_size,
